bcaee8d8626744436785b9c227368ba6043d2e7f
KateZzh <ekaterin.pro2@gmail.com>
2023-06-06 23:42:47 +0300

    add task.repository.js, task.service.js, task.controller.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = require('./src/app');
 
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`sercer is running on port ${port}`));
+app.listen(port, () => console.log(`server is running on port ${port}`));
