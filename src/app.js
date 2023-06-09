85d035a3c561eed728152b065053ded575365b68
KateZzh <ekaterin.pro2@gmail.com>
2023-06-09 23:00:08 +0300

    add auth route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const user = require('../src/controller/user.controller');
 const task = require('../src/controller/task.controller');
+const auth = require('../src/controller/auth.controller');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 app.use('/user', user);
 app.use('/task', task);
+app.use('/auth', auth);
 
 app.use((error, req, res, _next) => res.send(error.message));
 
